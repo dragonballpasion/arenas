1921dac4b841bafab3f174d94ede70e72135f43b
CamiloArenas <camiloarenas2712@gmail.com>
2024-11-07 20:31:29 -0300

    mondongo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class TablaMultiplicar
+{
+    public static void Main(string[] args)
+    {
+        int x = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine("{0}x{1}= {2}", x, i, x * i);
+        }
+        Console.ReadKey();  
+    }
+}
+
+/*
+ * Escribe un programa en C# que pregunte al usuario por un número x y muestre el resultado de su tabla de multiplicar del 1 al 10.
+ * */
